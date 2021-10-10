141bb63d73039bf07540c9848bd8e7c582c73129
Kunal Sharma <kushkunal077@gmail.com>
2021-10-10 21:34:04 +0530

    Create settings.py

---
+++ entries/settings.py
@@ -0,0 +1,11 @@
+# diary/settings.py
+
+INSTALLED_APPS = [
+  "entries.apps.EntriesConfig",
+  "django.contrib.admin",
+  "django.contrib.auth",
+  "django.contrib.contenttypes",
+  "django.contrib.sessions",
+  "django.contrib.messages",
+  "django.contrib.staticfiles",
+]
